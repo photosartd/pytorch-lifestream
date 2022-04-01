869d4955309e80ea0cf46309b5176e6fddb89a90
photosartd <dmitry.trofimow2011@gmail.com>
2022-04-01 09:26:18 +0300

    Faster diskfile added

--- alpha_data_load/diskfile.py
+++ alpha_data_load/diskfile.py
@@ -1,6 +1,7 @@
 import io
 import pickle as pkl
 
+import numpy as np
 from lz4 import frame
 
 
@@ -17,16 +18,31 @@ class DiskFile(object):
         return idx in self.index
 
     def __getitem__(self, idx):
-        if idx not in self:
-            raise KeyError(f'No item with such index: {idx}')
-        position, n_bytes = self.index[idx]
-        """
-        TODO: нужно переопределить слайсы, чтобы открывать файл только 1 раз 
-        для больших батчей и читать последовательно
-        """
-        with open(self.file_path, 'rb') as inp:
-            inp.seek(position, 0)
-            data = inp.read(n_bytes)
+        if isinstance(idx, list) or isinstance(idx, np.ndarray):
+            pos = []
+            bytes_list = []
+            result = []
+            for i in idx:
+                position, n_bytes = self.index[i]
+                pos.append(position)
+                bytes_list.append(n_bytes)
+            with open(self.file_path, 'rb') as inp:
+                for position, n_bytes in zip(pos, bytes_list):
+                    inp.seek(position, 0)
+                    data = inp.read(n_bytes)
+                    result.append(self.decompress_and_deserialize(data))
+            return result
+        else:
+            if idx not in self:
+                raise KeyError(f'No item with such index: {idx}')
+            position, n_bytes = self.index[idx]
+            """
+            TODO: нужно переопределить слайсы, чтобы открывать файл только 1 раз 
+            для больших батчей и читать последовательно
+            """
+            with open(self.file_path, 'rb') as inp:
+                inp.seek(position, 0)
+                data = inp.read(n_bytes)
         return self.decompress_and_deserialize(data)
 
 
